f7739ffbabe68acbd5f0845fd22f67b67fd8b14a
Jatin Chopra <81697765+Ja-tin@users.noreply.github.com>
2023-05-25 23:23:04 +0530

    Add files via upload

---
+++ function_practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printName(){
+  int n;
+  cin>>n;
+  for(int i=0; i<n; i++){
+    cout<<"Jatin"<<endl;
+  }
+}
+int main() {
+  
+  printName();
+  
+}
